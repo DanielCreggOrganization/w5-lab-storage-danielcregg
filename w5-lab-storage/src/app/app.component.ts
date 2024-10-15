15ec5dc2ccdcee715fcfcb26510c52cb8bbc006b
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-10-15 13:05:02 +0000

    Refactor AppComponent and StorageService to use dependency injection; remove direct initialization of StorageService

--- w5-lab-storage/src/app/app.component.ts
+++ w5-lab-storage/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-import { inject } from '@angular/core';
-import { StorageService } from './storage.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +8,7 @@ import { StorageService } from './storage.service';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  private storageService = inject(StorageService);
 
-  constructor() {}
+  constructor() { }
 
-  // Initializing the StorageService in the ngOnInit() lifecycle hook of the root component.
-  async ngOnInit() {
-    await this.storageService.init();
-  }
 }
--- w5-lab-storage/src/app/storage.service.ts
+++ w5-lab-storage/src/app/storage.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
-  constructor(private storage: Storage) {
+  private storage = inject(Storage);
+  
+  constructor() {
     this.init();
   }
 
